24ac09f849a8216236a1c48ca62031a4ed228d7d
Dmytro Blinnikov <dblinn@softserveinc.com>
2023-03-09 18:55:25 +0200

    New line added for better readability

--- main.py
+++ main.py
@@ -115,6 +115,7 @@ def main():
 
     # Setting up Planet instances
     sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30, 0, True)
+
     # Ok, ok... Sun isn't a Planet.. don't yell on me..
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GRAY, 3.30 * 10**23, -47.4 * 1000)
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24, -35.02 * 1000)
